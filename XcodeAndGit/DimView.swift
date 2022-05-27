931f4a334f7935f355122bb5743a5c784c9aac66
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-27 16:42:48 +0200

    Add DimView

---
+++ XcodeAndGit/DimView.swift
@@ -0,0 +1,20 @@
+//
+//  DimView.swift
+//  XcodeAndGit
+//
+//  Created by dimitri on 27/05/2022.
+//
+
+import SwiftUI
+
+struct DimView: View {
+    var body: some View {
+        Text("Ecran de Dim")
+    }
+}
+
+struct DimView_Previews: PreviewProvider {
+    static var previews: some View {
+        DimView()
+    }
+}
