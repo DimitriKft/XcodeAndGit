37eb4aab4a97e668b581f36b47ccac4f617fcc0c
DimAFP <sangodim@gmail.com>
2022-05-27 17:59:07 +0200

    emi

---
+++ XcodeAndGit/EmiView.swift
@@ -0,0 +1,20 @@
+//
+//  EmiView.swift
+//  XcodeAndGit
+//
+//  Created by Apprenant 08 on 27/05/2022.
+//
+
+import SwiftUI
+
+struct EmiView: View {
+    var body: some View {
+        Text("Emi")
+    }
+}
+
+struct EmiView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmiView()
+    }
+}
