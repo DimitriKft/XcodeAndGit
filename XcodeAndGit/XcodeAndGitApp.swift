cb7c5bf99b949fe23c80b4c50b65d4b2751c13d1
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-27 15:08:14 +0200

    Initial Commit

---
+++ XcodeAndGit/XcodeAndGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeAndGitApp.swift
+//  XcodeAndGit
+//
+//  Created by dimitri on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeAndGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
